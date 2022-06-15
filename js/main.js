dc33f903776d3451a72a37a6ecb4acc1ed8e2072
Egor Besedin <Besedinegor@mail.ru>
2022-06-15 12:16:24 +0300

    функции от Кекс

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
-//Получение случайного целого числа из переданного диапазона
-//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+function getRandomPositiveInteger (a, b) {
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
+
+  // После нам нужно убедиться, что пользователь не передал дробные значения,
+  // для этого на всякий пожарный случай нижнюю границу диапазона
+  // мы округляем к ближайшему большему целому с помощью Math.ceil,
+  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
+  // мы не ругаем пользователя за переданное отрицательное число,
+  // а просто берём его по модулю с помощью Math.abs
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower + 1) + lower;
+  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
+
+  // И в конце с помощью метода Math.floor мы округляем полученный результат,
+  // потому что Math.random() генерирует только дробные числа и ноль.
+  return Math.floor(result);
 }
-getRandomInt(0, 10);
+getRandomPositiveInteger();
 
-//Проверка максималтной длины строки
-
-function checkingMaxString(string, maxLength) {
-  if (string.length <= maxLength) {
-    console.log('Строка проходит по длине');
-  } else {
-    console.log('Строка выходит за пределы допустимой длины');
-  }
-  // string.length <= maxLength ? console.log('Строка проходит по длине') : console.log('Строка выходит за пределы допустимой длины');
+//Функция для проверки длины строки
+function checkStringLength (string, length) {
+  return string.length <= length;
 }
-
-checkingMaxString('hello World!', 140);
+checkStringLength();
