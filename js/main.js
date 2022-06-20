f51fba4990aa81855f9e13ec97bea14e791ffab0
Egor Besedin <Besedinegor@mail.ru>
2022-06-20 17:33:32 +0300

    массив объектов с повторениями элементов публикации

--- js/main.js
+++ js/main.js
@@ -1,35 +1,73 @@
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const NAME_LIST = [
+  'Арсений',
+  'Егор',
+  'Алексей',
+  'Евгений',
+  'Анастасия',
+  'Татьяна',
+  'Людмила',
+  'Павел',
+  'Максим',
+  'Владимир',
+];
 
-function getRandomPositiveInteger (a, b) {
-  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
-  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
-  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
+const DESCRIPTION_LIST = [
+  'Отлично проведённое время',
+  'Путь, который мы выбрали',
+  'Вот так встречают закаты и рассветы',
+  'Это лучшее, что могло произойти с нами за всё время'
+];
 
-  // После нам нужно убедиться, что пользователь не передал дробные значения,
-  // для этого на всякий пожарный случай нижнюю границу диапазона
-  // мы округляем к ближайшему большему целому с помощью Math.ceil,
-  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.'
+];
+
+// Функция возвращающая случайное целое число из переданного диапазона включительно
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random + доработка
+
+const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
-  // мы не ругаем пользователя за переданное отрицательное число,
-  // а просто берём его по модулю с помощью Math.abs
-
-  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
-  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
-  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
   const result = Math.random() * (upper - lower + 1) + lower;
-  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
-
-  // И в конце с помощью метода Math.floor мы округляем полученный результат,
-  // потому что Math.random() генерирует только дробные числа и ноль.
   return Math.floor(result);
-}
+};
 getRandomPositiveInteger();
 
-//Функция для проверки длины строки
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
-checkStringLength();
+//проверка длины строки
+const checkStringLength = (string, length) => string.length <= length;
+
+//возврат случайного элемента массива
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+// объект для настройки генерирующих публикаций
+const generationObjectPublications = () => {
+  const getNumberLikes = getRandomPositiveInteger(15, 200);
+
+  return {
+    publicationIndex: getRandomPositiveInteger(1, 25),
+    publicationUrl: `photos/${getRandomPositiveInteger(1, 25)}.jpg`,
+    publicationDescription: getRandomArrayElement(DESCRIPTION_LIST),
+    likesNumber: getNumberLikes,
+    commentsPublication: generationComments(),
+  };
+};
+
+// генерация комментариев
+const generationComments = () => {
+  const getRandomNames = getRandomArrayElement(NAME_LIST);
+
+  return {
+    id: getRandomPositiveInteger(1, 350),
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: getRandomArrayElement(MESSAGE),
+    userName: getRandomNames
+  };
+};
+
+//генерация публикаций
+const generationRandomPublication = Array.from({length: 25}, generationObjectPublications);
+// console.log(generationRandomPublication);
+
+
