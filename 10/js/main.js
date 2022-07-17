8d2a6ed8c04898fbdc79de63d5a1bc338bd8b121
Keks <keks@htmlacademy.ru>
2022-07-17 18:52:38 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,11 @@
+import {generateRandomPublication} from './modules/data.js';
+
+import {renderingPhotoPublication} from './modules/rendering-publications.js';
+
+import './modules/popup-photo.js';
+
+import './modules/validation-form.js';
+
+//генерируемые публикации в main
+const publications = generateRandomPublication(25);
+renderingPhotoPublication(publications);
